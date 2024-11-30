154805d31bc3eb93198cfc626e0a8d8ba4f7f34e
SpringForever-2007 <jxh445381200712315711@outlook.com>
2024-11-30 12:01:27 +0800

    添加项目文件。

---
+++ Class1.cs
@@ -0,0 +1,66 @@
+﻿using System.Runtime.InteropServices;
+
+namespace RandomNet
+{
+    /// <summary>
+    /// the random class whitch use std c++ to make random numbers
+    /// the generators is mt19937,the seed is the local time
+    /// </summary>
+    public partial class RandomCppLibWrapper
+    {
+        /// <summary>
+        /// make normal random numbers
+        /// </summary>
+        /// <param name="arr">the results</param>
+        /// <param name="mean">the expected mean</param>
+        /// <param name="std_dev">the expected standard deviation</param>
+        /// <param name="range">the range of results</param>
+        unsafe public void MakeNormalRandom(int[] arr,int mean,int std_dev,Range range)
+        {
+            fixed (int* arrPtr = arr)
+            {
+                make_normal_random(arrPtr, arr.Length, mean, std_dev, range.Start.Value, range.End.Value);
+            }
+        }
+
+        /// <summary>
+        /// make uniform random numbers
+        /// </summary>
+        /// <param name="arr">the results</param>
+        /// <param name="range">the range of results</param>
+        unsafe public void MakeUniformRandom(int[] arr,Range range)
+        {
+            fixed(int* arrPtr = arr)
+            {
+                make_uniform_random(arrPtr,arr.Length,range.Start.Value, range.End.Value);
+            }
+        }
+
+        /// <summary>
+        /// make common random numbers
+        /// </summary>
+        /// <param name="arr">the results</param>
+        /// <param name="range">the range of results</param>
+        unsafe public void MakeRandom(int[] arr,Range range)
+        {
+            fixed(int* arrPtr = arr)
+            {
+                make_random(arrPtr,arr.Length,range.Start.Value,range.End.Value);
+            }
+        }
+
+        /// <summary>
+        /// make double random numbers
+        /// </summary>
+        /// <param name="arr">the results</param>
+        /// <param name="min">the minimum</param>
+        /// <param name="max">the maximum</param>
+        unsafe public void MakeFloatingRandom(double[] arr, double min, double max)
+        {
+            fixed(double* arrPtr = arr)
+            {
+                make_floating_random(arrPtr, arr.Length, min, max);
+            }
+        }
+    }
+}
---
+++ Class2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNet
+{
+    partial class RandomCppLibWrapper
+    {
+        //dll import
+        [DllImport("randomcpplib.dll", CallingConvention = CallingConvention.Cdecl)]
+        unsafe protected static extern void exchange(int* a, int* b);
+
+        [DllImport("randomcpplib.dll", CallingConvention = CallingConvention.Cdecl)]
+        unsafe protected static extern void exchange_f(double* a, double* b);
+
+        [DllImport("randomcpplib.dll", CallingConvention = CallingConvention.Cdecl)]
+        unsafe protected static extern void check_max_min(int* max, int* min);
+
+        [DllImport("randomcpplib.dll", CallingConvention = CallingConvention.Cdecl)]
+        unsafe protected static extern void check_max_min_f(double* max, double* min);
+
+        [DllImport("randomcpplib.dll", CallingConvention = CallingConvention.Cdecl)]
+        unsafe protected static extern bool has_same(int* arr, int size, int dest);
+
+        [DllImport("randomcpplib.dll", CallingConvention = CallingConvention.Cdecl)]
+        unsafe protected static extern bool has_same_f(double* arr, int size, double dest);
+
+        [DllImport("randomcpplib.dll", CallingConvention = CallingConvention.Cdecl)]
+        unsafe protected static extern void make_normal_random(int* arr, int size, int mean, int std_dev, int min, int max);
+
+        [DllImport("randomcpplib.dll", CallingConvention = CallingConvention.Cdecl)]
+        unsafe protected static extern void make_uniform_random(int* arr, int size, int min, int max);
+
+        [DllImport("randomcpplib.dll", CallingConvention = CallingConvention.Cdecl)]
+        unsafe protected static extern void make_random(int* arr, int size, int min, int max);
+
+        [DllImport("randomcpplib.dll", CallingConvention = CallingConvention.Cdecl)]
+        unsafe protected static extern void make_floating_random(double* arr, int size, double min, double max);
+    }
+}
